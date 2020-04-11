b609a7ec3f8fe032e05a98b3b2fa541bd48f1dd7
LeviHarrison <levisamuelharrison@gmail.com>
2020-04-11 17:46:06 -0400

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './main.css'
+
+export class NetParty extends React.Component {
+    render() {
+        return (
+            <div className="maindiv">
+            <h1 className="main">NetParty</h1>
+            </div>
+        )
+    }
+}
