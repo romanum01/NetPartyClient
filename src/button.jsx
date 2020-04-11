a406d86abc51a8e703ca417c1cb5630bb2fcdb67
LeviHarrison <levisamuelharrison@gmail.com>
2020-04-11 17:59:37 -0400

    added zeit stuff

--- src/button.jsx
+++ src/button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export class Button extends React.Component {
     render() {
         return (
-            <
+            <div onClick={this.state.page}
         )
     }
 }
